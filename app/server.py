c30f667530186502925c6410bc11f2c076e87716
NoisyPunk <inismonabs@yandex.ru>
2020-05-07 21:03:42 +0300

    Создан сервер и клинтский протокол

---
+++ app/server.py
@@ -0,0 +1,52 @@
+"""Серверное приложение"""
+import asyncio
+from asyncio import transports
+from typing import Optional
+
+
+class ClientProtocol(asyncio.Protocol):
+    login: str
+    server: 'Server' # Не строка а тип класса
+    transport: transports.Transport
+
+    def __int__(self, server: 'Server'):
+        self.server = server
+
+    def data_received(self, data: bytes):
+        print(data)
+
+
+    def connection_made(self, transport: transports.Transport):
+        self.transport = transport
+        print("Соединение установлено")
+
+
+    def connection_lost(self, exception):
+    print("Соединение разорвано")
+
+
+class Server:
+    clients: list
+
+    def __init__(self):
+        self.clients = []
+
+    def create_protocol(self):
+        return ClientProtocol(self)
+
+    def start(self):
+        loop = asyncio.get_running_loop()
+
+        coroutine = await loop.create_server(
+            self.create_protocol,
+            "127.0.0.1",
+            8888
+        )
+
+        print("Сервер запущен ...")
+
+        await coroutine.serve_forever()
+
+
+process = Server()
+asyncio.run(process.start())
